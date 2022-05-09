0ee1d9610dbbca5d613fe32e0c275b072855073e
menoutep <99316473+menoutep@users.noreply.github.com>
2022-05-09 11:14:59 +0000

    Update text.py

--- text.py
+++ text.py
@@ -2,7 +2,7 @@ filename = input("entrez le nom du fichier texte : ")
 file = open(filename, "r")
 tab=list()
 for line in file:
-    print(line)
+    print(line)# cette ligne permet d'afficher chaque ligne du fichier texte si vous ne desiserz pas afficher le fichier texte, supprimez simplement cette ligne  
     tab.append(line)
 file.close()
 i = int()
